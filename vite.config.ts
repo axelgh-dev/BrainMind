017eb12958dc3b8acf485c881ec3ec3c892f71b8
Axel Hernandez <axel@Mac-mini-de-Axel.local>
2026-07-27 10:51:16 -0600

    [FIX] Nitro plugin fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
+    nitro({
+      preset: 'vercel',
+    }),
     viteReact(),
   ],
 })
