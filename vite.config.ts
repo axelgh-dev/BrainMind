326fbdea7f03b1d7cd08decd16dae20fbd47d9bd
Axel Hernandez <axel@Mac-mini-de-Axel.local>
2026-07-27 10:42:36 -0600

    [FIX] vite and package config fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -12,11 +11,7 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
-    // Orden importante: tanstackStart() -> nitro() -> viteReact()
     tanstackStart(),
-    nitro({
-      preset: 'vercel',
-    }),
     viteReact(),
   ],
 })
